97f3325ac14b900b78fab4e8225cf0fdd18137cb
bzebosi <zebosibrian@gmail.com>
2025-01-16 11:26:39 -0500

    Install muitiple packages at once

--- bsa_Rscript/multi_package_installer.R
+++ bsa_Rscript/multi_package_installer.R
@@ -38,16 +38,4 @@ Install_multi_package_bz <- function(packages) {
   message("All packages are loaded successfully.")
 }
 
-# Define the required packages
-packages <- c(
-  "reshape2", "ggpubr", "grid", "multcomp", "car", "Rmisc", "nlme", "minqa", "numDeriv", "GGally",
-  "nloptr", "RcppEigen", "ggsignif", "readxl", "dendsort", "viridis", "circlize", "BiocManager",
-  "zoo", "mvtnorm", "TH.data", "sandwich", "estimability", "plyr", "GlobalOptions", "shape","scales",
-  "SparseM", "MatrixModels", "carData", "abind", "pbkrtest", "quantreg", "corrplot", "car", "ggrepel",
-  "ggsci", "ggsignif", "gridExtra", "polynom", "rstatix", "tidyverse", "ComplexHeatmap", "emmeans",
-  "lme4", "Matrix", "lmerTest", "multcompView", "FSA", "patchwork", "openxlsx", "stringr", "vcfR",
-  "magrittr", "VariantAnnotation", "data.table", "naturalsort", "locfit", "rlang", "purrr", "gtools"
-)
 
-# Use the function to install and load packages
-Install_multi_package_bz(packages)
