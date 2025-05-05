550ea7e7e18d49fdbcaa9cb252277745527ec450
Zebosi Brian <31706621+bzebosi@users.noreply.github.com>
2025-05-05 15:20:16 -0700

    Update BSAseq_analysis.R

--- bsa_Rscript/BSAseq_analysis.R
+++ bsa_Rscript/BSAseq_analysis.R
@@ -302,7 +302,7 @@ generate_vcfplots <- function(data, prefix, column, y_title, plot_title, file_su
     
     # Add geom_point only if is_smooth is FALSE
     if (is_smooth) {
-      plot <- plot + stat_smooth(method = "locfit", formula = y ~ lp(x, nn = nn_prop))
+      plot <- plot + stat_smooth(method = "locfit", formula = y ~ lp(x, nn = nn_prop), size = 6)
     } else if (is_rollmedian) {
       plot <- plot + geom_point(size = 0.5) + 
         geom_line(aes(y = rollmedian(!!sym(column), rollmedian, na.pad = TRUE)), color = "black", linewidth = 30)
