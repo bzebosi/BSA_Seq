776a3410be161470904e5edc9135a7412862d52a
Zebosi Brian <31706621+bzebosi@users.noreply.github.com>
2025-05-05 13:18:41 -0700

    Update generate_vcfplots.R

--- bsa_Rscript/generate_vcfplots.R
+++ bsa_Rscript/generate_vcfplots.R
@@ -43,7 +43,6 @@ generate_vcfplots <- function(data, prefix, column, y_title, plot_title, file_su
   if(!is_histogram){
     plot <- ggplot(data, aes(x = POS, y = !!sym(column), color = CHROM)) +
       facet_grid(. ~ CHROM, scales = "free_x", space = "free_y") +
-      scale_y_continuous(labels = scales::number_format(accuracy = 0.1)) +
       scale_x_continuous(breaks = seq(min(data$POS), max(data$POS), by = 20000000), labels = scales::comma) +
       scale_color_manual(values = color_panel) + guides(color = FALSE) +
       labs(title = paste0("Aligned to ", inbred, " : ", " ", plot_title), x = "\n Chromosome Position (bp) \n", y = paste0("\n", y_title, "\n")) + bsa_theme()
@@ -59,12 +58,12 @@ generate_vcfplots <- function(data, prefix, column, y_title, plot_title, file_su
     }
     
     # Add horizontal threshold line if applicable
-    if (!is.null(threshold)) {plot <- plot + geom_hline(yintercept = threshold, linetype = "dashed", color = "black", linewidth = 1.2)}
+    if (!is.null(threshold)) {plot <- plot + geom_hline(yintercept = threshold, linetype = "dashed", color = "black", linewidth = 4)}
     
   } else {
     plot <- ggplot(data, aes(x=POS, fill=CHROM)) + geom_histogram(binwidth=bwidth,  alpha = 0.7) +
       facet_wrap(~ CHROM, ncol = 5, scales = "free_x") + scale_fill_manual(values = color_panel) +
-      theme(axis.ticks.x  = element_line(color = "black", linewidth = 1),
+      theme(axis.ticks.x  = element_line(color = "black", linewidth = 4),
             axis.minor.ticks.length = unit(0.5, "cm"), axis.text.x = element_blank()) + guides(fill = FALSE) +
       labs(title = paste0("Aligned to ", inbred, " : ", " ", plot_title), x = "\n Chromosome Position (bp) \n", y = paste0("\n", y_title, "\n")) + bsa_theme()
   }
