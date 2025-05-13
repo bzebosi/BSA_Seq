4dcd6b1fc1ac8555f0adb4acaca56331882795af
Zebosi Brian <31706621+bzebosi@users.noreply.github.com>
2025-05-13 13:12:44 -0700

    Update BSAseq_analysis.R

--- bsa_Rscript/BSAseq_analysis.R
+++ bsa_Rscript/BSAseq_analysis.R
@@ -295,17 +295,14 @@ generate_vcfplots <- function(data, prefix, column, y_title, plot_title, file_su
   # Generate scatter or smooth plot or histogram
   if(!is_histogram){
     plot <- ggplot(data, aes(x = POS, y = !!sym(column), color = CHROM)) +
-      facet_grid(. ~ CHROM, scales = "free_x", space = "free_y") +
+      facet_wrap(~ CHROM, ncol = 5, scales = "free_x") +
       scale_x_continuous(breaks = seq(min(data$POS), max(data$POS), by = 20000000), labels = scales::comma) +
       scale_color_manual(values = color_panel) + guides(color = FALSE) +
       labs(title = paste0("Aligned to ", inbred, " : ", " ", plot_title), x = "\n Chromosome Position (bp) \n", y = paste0("\n", y_title, "\n")) + bsa_theme()
     
     # Add geom_point only if is_smooth is FALSE
     if (is_smooth) {
-      plot <- plot + stat_smooth(method = "locfit", formula = y ~ lp(x, nn = nn_prop), size = 3) +
-      facet_grid(. ~ CHROM, scales = "free_x", space = "free_y") +
-      scale_x_continuous(breaks = seq(min(data$POS), max(data$POS), by = 20000000), labels = scales::comma) +
-      scale_color_manual(values = color_panel) + guides(color = FALSE)
+      plot <- plot + stat_smooth(method = "locfit", formula = y ~ lp(x, nn = nn_prop), size = 3)
     } else if (is_rollmedian) {
       plot <- plot + geom_point(size = 0.5) + 
         geom_line(aes(y = rollmedian(!!sym(column), rollmedian, na.pad = TRUE)), color = "black", linewidth = 3)
