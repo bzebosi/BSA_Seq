6ee746b4a13482db9b5c5c6ff5dc86b644fcb50d
bzebosi <zebosibrian@gmail.com>
2025-07-29 09:28:34 -0700

    analyze and visualize vcfdata function - multiple

---
+++ bsa_Rscript/run_bsa_wrapper.R
@@ -0,0 +1,19 @@
+# Load all required functions from GitHub
+source("https://raw.githubusercontent.com/bzebosi/BSA_Seq/main/bsa_Rscript/multi_package_installer.R")
+source("https://raw.githubusercontent.com/bzebosi/BSA_Seq/main/bsa_Rscript/bsa_theme.R")
+source("https://raw.githubusercontent.com/bzebosi/BSA_Seq/main/bsa_Rscript/import_vcfdata.R")
+source("https://raw.githubusercontent.com/bzebosi/BSA_Seq/main/bsa_Rscript/analyze_vcfdata.R")
+source("https://raw.githubusercontent.com/bzebosi/BSA_Seq/main/bsa_Rscript/plot_vcfdata.R")
+source("https://raw.githubusercontent.com/bzebosi/BSA_Seq/main/bsa_Rscript/run_af_only.R")
+source("https://raw.githubusercontent.com/bzebosi/BSA_Seq/main/bsa_Rscript/run_ed_only.R")
+source("https://raw.githubusercontent.com/bzebosi/BSA_Seq/main/bsa_Rscript/run_g_only.R")
+source("https://raw.githubusercontent.com/bzebosi/BSA_Seq/main/bsa_Rscript/run_histogram_only.R")
+source("https://raw.githubusercontent.com/bzebosi/BSA_Seq/main/bsa_Rscript/run_simpval_only.R")
+source("https://raw.githubusercontent.com/bzebosi/BSA_Seq/main/bsa_Rscript/run_bsa_pipeline.R")
+
+# Install required packages
+packages <- c(
+  "reshape2", "readxl", "BiocManager", "zoo", "plyr", "GlobalOptions", "shape", "scales",
+  "tidyverse", "openxlsx", "stringr", "IRanges", "magrittr", "data.table", "naturalsort", "locfit", "rlang"
+)
+Install_multi_package_bz(packages)
