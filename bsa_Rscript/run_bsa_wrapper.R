7e0bb2cb19a2e40ebc46597aabe65f3a2e20d9f7
bzebosi <zebosibrian@gmail.com>
2025-07-29 09:33:10 -0700

    bsa analysis wrapper

--- bsa_Rscript/run_bsa_wrapper.R
+++ bsa_Rscript/run_bsa_wrapper.R
@@ -12,9 +12,11 @@ source("https://raw.githubusercontent.com/bzebosi/BSA_Seq/main/bsa_Rscript/run_s
 source("https://raw.githubusercontent.com/bzebosi/BSA_Seq/main/bsa_Rscript/run_bsa_pipeline.R")
 source("https://raw.githubusercontent.com/bzebosi/BSA_Seq/main/bsa_Rscript/run_bsa_auto.R")
 
-# Install required packages
+# required packages
 packages <- c(
   "reshape2", "readxl", "BiocManager", "zoo", "plyr", "GlobalOptions", "shape", "scales",
   "tidyverse", "openxlsx", "stringr", "IRanges", "magrittr", "data.table", "naturalsort", "locfit", "rlang"
 )
+
+# Install required packages
 Install_multi_package_bz(packages)
