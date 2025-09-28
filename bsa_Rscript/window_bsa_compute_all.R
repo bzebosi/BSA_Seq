0df963977d44f12bb1c990e43d644dd22443d8e3
bzebosi <zebosibrian@gmail.com>
2025-09-28 13:27:43 -0700

    window_bsa_compute_all.R

---
+++ bsa_Rscript/window_bsa_compute_all.R
@@ -0,0 +1,69 @@
+window_bsa_compute_all <- function(data, metrics = c("af","ed","ed4","afd","g","all"),
+                               af_col = "mt_AF", use_ems = FALSE, only_mutant = FALSE,
+                               window_size = 2e6, step_size = 1e5,rollmedian = 100L, nn_prop = 0.1,
+                               find_intervals = TRUE, offhold = 0.80, min_vsize = 1e6){
+  
+  metrics <- tolower(metrics)
+  if ("all" %in% metrics) metrics <- c("af","ed","ed4","afd","g")
+  
+  out <- list(windows = list(), intervals = list())
+  
+  
+  af_cols <- if ("both" %in% af_col) c("mt_AF","wt_AF") else af_col
+  
+  if ("af" %in% metrics) {
+    for (fq in af_cols) {
+      if (!fq %in% c("mt_AF","wt_AF")) stop("af_col must be 'mt_AF', 'wt_AF', or 'both'.")
+      if (only_mutant && fq == "wt_AF") { message("only_mutant=TRUE: skipping wt_AF."); next }
+      
+      # choose table name based on AF side + EMS flag
+      doi <- if (fq == "mt_AF") {
+        paste0("ant_mt", if (use_ems) "_ems")
+      } else {
+        paste0("ant_wt", if (use_ems) "_ems")
+      }
+      
+      dt <- data[[doi]]
+      if (is.null(dt)) stop("Missing table: ", doi)
+      if (!fq %in% names(dt)) stop("Column ", fq, " not found in ", doi)
+      
+      res <- window_bsa_compute(data = dt, metric_col = fq, window_size = window_size, 
+                                step_size = step_size, rollmedian = rollmedian, 
+                                nn_prop = nn_prop, find_intervals = find_intervals, 
+                                offhold = offhold, min_vsize = min_vsize, use_cols = c()
+      )
+      
+      out$windows[[fq]]   <- res$windows
+      out$intervals[[fq]] <- if ("intervals" %in% names(res)) res$intervals else NULL
+    }
+  }
+  
+  # ---- Joint metrics (from wt_mt) ----
+  joint <- unique(metrics[metrics %in% c("ed","ed4","afd","g")])
+  if (!only_mutant && length(joint)) {
+    wt_mt <- data$wt_mt
+    if (is.null(wt_mt)) stop("Missing table: wt_mt")
+    
+    col_map <- c(ed = "ED", ed4 = "ED4", afd = "AFD", g = "G")
+    for (m in joint) {
+      metric_col <- unname(col_map[[m]])
+      if (!metric_col %in% names(wt_mt)) 
+        stop("Column ", metric_col, " not found in wt_mt")
+      
+      res <- window_bsa_compute(
+        data = wt_mt, metric_col = metric_col,
+        window_size = window_size, step_size = step_size,
+        rollmedian = rollmedian, nn_prop = nn_prop,
+        find_intervals = find_intervals, offhold = offhold, min_vsize = min_vsize,
+        use_cols = c()
+      )
+      out$windows[[m]]   <- res$windows
+      out$intervals[[m]] <- if ("intervals" %in% names(res)) res$intervals else NULL
+    } 
+    
+  } else if (only_mutant && length(joint)) {
+    message("only_mutant=TRUE: skipping joint metrics (", paste(joint, collapse = ", "), ").")
+  }
+  
+  return(out)
+}
